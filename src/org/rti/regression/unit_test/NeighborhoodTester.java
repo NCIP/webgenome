c0991f5b106160ddc933f3ba50717928dce3eb02
Vesselina Bakalov <vbakalov@rti.org>
2005-12-14 20:47:57 +0000

    initial check-in
    SVN-Revision: 13112

---
+++ src/org/rti/regression/unit_test/NeighborhoodTester.java
@@ -0,0 +1,82 @@
+/*
+
+$Source: /share/content/gforge/webcgh/webgenome/src/org/rti/regression/unit_test/NeighborhoodTester.java,v $
+$Revision: 1.1 $
+$Date: 2005-12-14 20:47:36 $
+
+The Web CGH Software License, Version 1.0
+
+Copyright 2003 RTI. This software was developed in conjunction with the National 
+Cancer Institute, and so to the extent government employees are co-authors, any 
+rights in such works shall be subject to Title 17 of the United States Code, 
+section 105.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the disclaimer of Article 3, below. Redistributions in 
+binary form must reproduce the above copyright notice, this list of conditions 
+and the following disclaimer in the documentation and/or other materials 
+provided with the distribution.
+
+2. The end-user documentation included with the redistribution, if any, must 
+include the following acknowledgment:
+
+"This product includes software developed by the RTI and the National Cancer 
+Institute."
+
+If no such end-user documentation is to be included, this acknowledgment shall 
+appear in the software itself, wherever such third-party acknowledgments 
+normally appear.
+
+3. The names "The National Cancer Institute", "NCI", 
+“Research Triangle Institute”, and "RTI" must not be used to endorse or promote 
+products derived from this software.
+
+4. This license does not authorize the incorporation of this software into any 
+proprietary programs. This license does not authorize the recipient to use any 
+trademarks owned by either NCI or RTI.
+
+5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, 
+(INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO EVENT SHALL THE NATIONAL 
+CANCER INSTITUTE, RTI, OR THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.rti.regression.unit_test;
+
+import junit.framework.TestCase;
+
+import org.rti.regression.Neighbor;
+import org.rti.regression.Neighborhood;
+
+/**
+ * Tester for Neighborhood
+ */
+public class NeighborhoodTester extends TestCase {
+	
+	/**
+	 */		
+	public void testInsert() {
+		Neighbor n1 = new Neighbor(1, 0.5);
+		Neighbor n2 = new Neighbor(2, 1.5);
+		Neighbor n3 = new Neighbor(3, 0.75);
+		Neighborhood nh = new Neighborhood(2);
+		nh.insert(n2);
+		assertEquals(nh.numNeighbors(), 1);
+		nh.insert(n1);
+		assertTrue(nh.getNeighborAt(1) == n2);
+		nh.insert(n3);
+		assertTrue(nh.getNeighborAt(0) == n1);
+		assertTrue(nh.getNeighborAt(1) == n3);
+	}
+
+}
