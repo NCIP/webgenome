081293b9c2d195a8bc9e73eab13ffebed3822306
Vesselina Bakalov <vbakalov@rti.org>
2006-04-19 15:27:44 +0000

    no message
    SVN-Revision: 13140

--- src/org/rti/webcgh/array/AcghData.java
+++ src/org/rti/webcgh/array/AcghData.java
@@ -1,8 +1,8 @@
 /*
 
 $Source: /share/content/gforge/webcgh/webgenome/src/org/rti/webcgh/array/AcghData.java,v $
-$Revision: 1.1 $
-$Date: 2006-04-13 15:28:04 $
+$Revision: 1.2 $
+$Date: 2006-04-19 15:27:44 $
 
 The Web CGH Software License, Version 1.0
 
@@ -54,8 +54,101 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package org.rti.webcgh.array;
 
-public class AcghData {
-	
+import java.io.FileReader;
+import com.Ostermiller.util.ExcelCSVParser;
 
+public class AcghData {
+
+	private double[] log2Ratios;      // log2 ratios of copy number changes
+						              // rows correspond to the clones and columns to the samples
+	private String[] clones;          // clone name
+	private String[] targets;         // unique ID, e.g. Well ID
+	private int[] chromosomes;        // chromosome number
+	                                  // X chromosome = 23 in human and 20 in mouse,
+	                                  // Y chromosome = 24 in human and 21 in mouse
+	private int[] positions;          // kb position on the chromosome
+	private double[] smoothedRatios;  // smoothed value of log2 ratio
+	private int size;                 // number of clones/number of rows
+	
+	public AcghData() {
+		loadDataFromFiles();
+	}
+	
+	private void loadDataFromFiles() {
+		try {
+		    //read csv files
+		    System.out.print("Reading data files...");
+		    ExcelCSVParser ecp1 = new ExcelCSVParser(new FileReader("C:/test/data/T_GPR_R_aCGH_cloneinfo_NoDups.csv"));
+		    ExcelCSVParser ecp2 = new ExcelCSVParser(new FileReader("C:/test/data/T_GPR_R_aCGH_log2ratios_NoDups.csv"));
+		    System.out.println("OK");
+		    
+		    // create matrix table from files
+			System.out.print("Creating data matrix...");
+			String[][] cloneInfoFile = ecp1.getAllValues();
+			String[][] log2ratiosFile = ecp2.getAllValues();
+			System.out.println("OK");
+			
+			size = cloneInfoFile.length - 1; // fine line is header
+			log2Ratios = new double[size];
+			clones = new String[size];
+			targets = new String[size];
+			chromosomes = new int[size];
+			positions = new int[size]; 
+			smoothedRatios = new double[size];
+			
+			for(int i = 0 ; i < size ; i++) {
+				clones[i] = cloneInfoFile[i+1][1];
+				targets[i] = cloneInfoFile[i+1][2];
+				chromosomes[i] = Integer.parseInt(cloneInfoFile[i+1][3].trim());
+				positions[i] = Integer.parseInt(cloneInfoFile[i+1][4].trim());
+				if (!cloneInfoFile[i+1][1].equals(log2ratiosFile[i+1][0])) 
+				    {System.out.println("two files are not perfect match at "+i+"th clone.");}
+				log2Ratios[i] = Double.parseDouble(log2ratiosFile[i+1][1].trim());
+			}
+			/*
+			System.out.println(log2Ratios[0]);
+			System.out.println(clones[0]);
+			System.out.println(targets[0]);
+			System.out.println(chromosomes[0]);
+			System.out.println(positions[0]);
+			
+			System.out.println(log2Ratios[log2Ratios.length-1]);
+			System.out.println(clones[clones.length-1]);
+			System.out.println(targets[targets.length-1]);
+			System.out.println(chromosomes[chromosomes.length-1]);
+			System.out.println(positions[positions.length-1]);
+			*/
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public double[] getLog2Ratios() {
+	    return log2Ratios;
+	}
+	public String[] getClones() {
+	    return clones;
+	}
+	public String[] getTargets() {
+	    return targets;
+	}
+	public int[] getChromosomes() {
+	    return chromosomes;
+	}
+	public int[] getPositions() {
+	    return positions;
+	}
+	public double[] getSmoothedRatios() {
+	    return smoothedRatios;
+	}
+	public int getSize() {
+	    return size;
+	}
+	public void setSmoothedRatios(double[] smooth) {
+	    smoothedRatios = smooth;
+	}
 	
 }
