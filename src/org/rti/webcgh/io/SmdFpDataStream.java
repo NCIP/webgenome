990ed554c30b6c7eff8b3462418780fa7cb97f98
Vesselina Bakalov <vbakalov@rti.org>
2006-05-04 18:31:14 +0000

    First working version.
    SVN-Revision: 13158

--- src/org/rti/webcgh/io/SmdFpDataStream.java
+++ src/org/rti/webcgh/io/SmdFpDataStream.java
@@ -1,8 +1,8 @@
 /*
 
 $Source: /share/content/gforge/webcgh/webgenome/src/org/rti/webcgh/io/SmdFpDataStream.java,v $
-$Revision: 1.1 $
-$Date: 2006-04-25 16:28:26 $
+$Revision: 1.2 $
+$Date: 2006-05-04 18:31:14 $
 
 The Web CGH Software License, Version 1.0
 
@@ -51,7 +51,6 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-
 package org.rti.webcgh.io;
 
 import java.io.BufferedReader;
@@ -59,13 +58,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import org.rti.webcgh.array.BioAssay;
-import org.rti.webcgh.array.Experiment;
+import org.rti.webcgh.array.*;
 import org.rti.webcgh.core.WebcghSystemException;
-
+import org.rti.webcgh.service.DomainObjectFactory;
 
 /**
- * Convert stream of floating point SMD data into data objects.
+ * Convert a stream of floating point SMD (Stanford Microarray Database) data into data objects.
+ *
  * The following describes the SMD data file format:
  * 
  * (1) Data are delimited text (e.g., comma-separated values)
@@ -74,80 +73,224 @@ import org.rti.webcgh.core.WebcghSystemException;
  * (3) Each row, with the exception of the first (i.e., column headings),
  *     corresponds to a reporter (i.e., probe).
  * (4) The following are special columns:
- *         i.  The first column contains reporter names
+ *         i.  The first column contains reporter names (can have any column name)
  *         ii. OPTIONAL: The file may contain columns with
  *             headings named 'CHROMOSOME' and 'POSITION.'
  *             These contain the chromosome number and physical
  *             position (i.e., base pair), respectively,
  *             of the corresponding reporter.
  *             The column names may be lower case or upper case.
- * (5) The heading (i.e., first row) of each column gives the identifier
+ * (5) The heading (i.e., first row) of each column gives the identifier (name)
  *     of the corresponding bioassay.
  * (6) Values in bioassay columns give the measurements of the corresponding
- *     reporters.
- *     
- * Example:
- * 
- * NAME		CHROMOSOME		POSITION		BIOASSAY1		BIOASSAY2
- * RP-1		1				15000			0.3				-0.1
- * RP-2		1				34000			-0.2			0.5
+ *     reporters. Bioassay columns which follow can have any column name (see point 5 above).
+ *     There can be any number of them, but there must be at least one.
+ *
+ * <p>Example:</p>
+ * <pre>
+ * <NAME>	CHROMOSOME		POSITION		<BIOASSAY1>		<BIOASSAY2>  ...  <BIOASSAY n>
+ * RP-1		1				15000			0.3				-0.1         ...  0.2
+ * RP-2		1				34000			-0.2			0.5          ...  0.1
+ * </pre>
  *
  */
 public class SmdFpDataStream implements SmdDataStream {
-	
-	private static final String CHROMOSOME = "CHROMOSOME";
-	private static final String POSITION = "POSITION";
-		
-	
-	// ==============================
-	//        Constructors
-	// ==============================
-	
-	public SmdFpDataStream() {}
-	
-	// ==============================
-	//         Public methods
-	// ==============================
-	
-	/**
-	 * Load experiment from stream
-	 */
-	public Experiment loadExperiment(InputStream in) throws SmdFormatException {
-		Experiment exp = new Experiment();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		try {
-			
-			// Read headings and create bioassays
-			String line = reader.readLine();
-			if (line == null)
-				throw new SmdFormatException("File is empty");
-			BioAssay[] bioAssays = this.createBioAssays(line);
-			exp.add(bioAssays);
-			boolean havePositions = this.havePositions(line);
-			
-			// TODO: complete method
-			
-		} catch (IOException e) {
-			throw new WebcghSystemException("Error reading data stream", e);
-		}
-		return exp;
-	}
-	
-	
-	// ===============================
-	//         Private methods
-	// ===============================
-	
-	private BioAssay[] createBioAssays(String line) {
-		
-		// TODO: complete method
-		
-		return null;
-	}
-	
-	
-	private boolean havePositions(String line) {
-		String uc = line.toUpperCase();
-		return uc.indexOf(CHROMOSOME) >= 0 && uc.indexOf(POSITION) >= 0;
-	}
+
+    private static final String CHROMOSOME_COL_HEADING = "CHROMOSOME"; // optional, see Step 4.ii notes above
+    private static final String POSITION_COL_HEADING   = "POSITION";   // optional, see Step 4.ii notes above
+    // other columns could have any name
+
+    // Parser for extracting field values from a delimited line of text
+    private DelimitedLineParser dlp ;
+
+    // Factory for creating and caching domain objects
+    private DomainObjectFactory domainObjectFactory = new DomainObjectFactory();
+
+
+    // ==============================
+    //        Constructors
+    // ==============================
+
+    public SmdFpDataStream() {}
+
+    // ==============================
+    //         Public methods
+    // ==============================
+
+    /**
+     * Load experiment from an input stream containing SMD (Stanford Microarray Database) floating
+     * point data.
+     * @param in - InputStream containing the data
+     * @param qt - QuantitationType
+     * @param genomeAssembly
+     * @return Experiment - a representation of the the SMD input stream data, loaded into
+     * an Experiment comprising BioAssays, ArrayDatums, Reporters et al.
+     * @throws SmdFormatException
+     */
+    public Experiment loadExperiment( InputStream in,
+                                      QuantitationType qt,
+                                      GenomeAssembly genomeAssembly ) throws SmdFormatException {
+
+        Experiment exp = new Experiment() ;
+        BufferedReader reader = null ;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(in)) ;
+
+            //
+            //   Read the headings
+            //
+            String line = reader.readLine() ;
+            if (line == null)
+                throw new SmdFormatException("File is empty");
+            // Establish a line field parser, passing in the first line which contains the headings
+            dlp = new DelimitedLineParser ( line ) ;
+
+            boolean havePositions = this.havePositions( dlp ) ;
+            int bioAssayBeginIdx =  havePositions ? 3 : 1 ; // which column does our bio assay data start from?
+
+            BioAssay[] bioAssays = createBioAssays ( dlp.getColumnNames(), bioAssayBeginIdx ) ; // create our bio assay "buckets"
+
+            //
+            //    Read the contents of the SMD stream, line by line
+            //
+            while ( ( line = reader.readLine() ) != null ) {
+                String name = dlp.getProperty( line, 0 ) ; // could be any column name, so we just get position zero
+                String[] bioAssayFields = dlp.getProperties( line, bioAssayBeginIdx ) ;
+
+                if ( ! isEmpty ( name ) &&
+                       bioAssayFields.length > 0 ) { // only process it if we have some data
+
+                    // NAME field value --> Reporter (reporter might be cached if seen before)
+                    Reporter reporter = domainObjectFactory.getReporter( name ) ;
+
+                    if ( havePositions && ! reporter.isMapped ( genomeAssembly )) {
+                        ReporterMapping reporterMapping = createReporterMapping ( line, genomeAssembly, reporter ) ;
+                        reporter.setReporterMapping(reporterMapping) ;
+                    }
+
+                    populateBioAssays ( bioAssays, bioAssayFields, qt, reporter ) ;
+                }
+            }
+            exp.add ( bioAssays ) ;
+
+        } catch (IOException e) {
+            throw new WebcghSystemException("Error reading data stream", e) ;
+        } finally {
+            //
+            // Close up shop
+            //
+            if ( reader != null )
+                try {
+                    reader.close() ;
+                } catch ( IOException ignored ) {
+                    // TODO: maybe should log this as a Warning?
+                }
+        }
+        return exp;
+    }
+
+
+    // ===============================
+    //         Private methods
+    // ===============================
+
+    /**
+     * Create the initial BioAssays and assign them their names.
+     * @param allColumnNames
+     * @return Empty BioAssay array for subsequent population
+     */
+    private BioAssay[] createBioAssays ( String[] allColumnNames,
+                                         int bioAssayBeginIdx ) {
+        // Pare down the bioAssayNames from the set of all column names
+        BioAssay[] bioAssays = new BioAssay[ allColumnNames.length - bioAssayBeginIdx ] ;
+
+        // Add BioAssay placeholders for subsequent population (see populateBioAssays)
+        int bioAssayIdx = 0 ;
+        for ( int i = bioAssayBeginIdx; i < allColumnNames.length ; i++ ) {
+            BioAssay bioAssay = new BioAssay () ;
+            bioAssay.setName( allColumnNames [i] );
+            BioAssayData bad = new BioAssayData() ;
+            bioAssay.setBioAssayData( bad );
+            bioAssays[bioAssayIdx++] = bioAssay ;
+        }
+        return bioAssays ;
+    }
+
+    /**
+     * Create an ArrayDatum from a line field and add it to its correspdonding BioAssay column.
+     * There is a BioAssay object for each column in the SMD file with a name that matches the column.
+     *
+     * This method is responsible for:
+     * 1/ making a Quantitation from the BioAssay data field
+     * 2/ making an ArrayDatum from this, by combining it with its associated Reporter
+     * 3/ Adding this ArrayDatum to the Array of ArrayDatums kept for each BioAssay column
+     *
+     * @param bioAssays
+     * @param bioAssayFields
+     * @param qt
+     * @param reporter
+     */
+    private void populateBioAssays ( BioAssay[] bioAssays,
+                                     String[] bioAssayFields,
+                                     QuantitationType qt,
+                                     Reporter reporter ) {
+
+        for ( int i = 0 ; i < bioAssayFields.length ; i++ ) {
+            try {
+                double bioAssayRatio = Double.valueOf( bioAssayFields[i] ) ;
+
+                Quantitation quantitation = new Quantitation( (float) bioAssayRatio, qt ) ;
+                ArrayDatum datum = new ArrayDatum( reporter, quantitation ) ;
+
+                bioAssays[i].getBioAssayData().add( datum ) ;
+            }
+            catch ( NumberFormatException currentlyIgnored ) {
+                // should this be logged
+            }
+        }
+    }
+
+    /**
+     * Create a reporter mapping for our chromosome/position
+     * @param line
+     * @param genomeAssembly
+     * @param reporter
+     * @return ReporterMapping
+     */
+    private ReporterMapping createReporterMapping ( String line,
+                                                    GenomeAssembly genomeAssembly,
+                                                    Reporter reporter ) {
+        double chromosome = dlp.getNumericProperty( line, CHROMOSOME_COL_HEADING ) ;
+        double position =   dlp.getNumericProperty( line, POSITION_COL_HEADING ) ;
+
+        Chromosome chromosomeObj = domainObjectFactory.getChromosome (
+                genomeAssembly, (short) chromosome ) ;
+        GenomeLocation genomeLocation = new GenomeLocation(chromosomeObj, (long) position ) ;
+        return new ReporterMapping(reporter, genomeLocation) ;
+    }
+
+    /**
+     * Test whether the chromosone number and physical
+     * position (i.e. base pair) are present in the input stream. Their corresponding
+     * column headings will be present in the first line of the input stream (we use
+     * the delimited line parser class for determining this).
+     * @return true - if present, otherwise false.
+     */
+    private boolean havePositions( DelimitedLineParser dlp ) {
+        return dlp.hasColumn ( CHROMOSOME_COL_HEADING ) &&
+               dlp.hasColumn ( POSITION_COL_HEADING ) ;
+    }
+
+    /**
+     * Test whether a value is empty.
+     * @param value
+     * @return true, if the value is empty or null, false otherwise
+     */
+    private boolean isEmpty ( String value ) {
+        boolean isEmpty = true ;
+        if ( value != null && value.trim().length() > 0 )
+            isEmpty = false ;
+        return isEmpty ;
+    }
 }
