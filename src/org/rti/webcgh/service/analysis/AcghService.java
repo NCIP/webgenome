235e606a5056defd574f6d16718344c2219cb4c6
Vesselina Bakalov <vbakalov@rti.org>
2006-10-13 20:29:42 +0000

    commet added
    SVN-Revision: 13354

--- src/org/rti/webcgh/service/analysis/AcghService.java
+++ src/org/rti/webcgh/service/analysis/AcghService.java
@@ -1,8 +1,8 @@
 /*
 
 $Source: /share/content/gforge/webcgh/webgenome/src/org/rti/webcgh/service/analysis/AcghService.java,v $
-$Revision: 1.1 $
-$Date: 2006-09-07 18:54:53 $
+$Revision: 1.2 $
+$Date: 2006-10-13 20:29:42 $
 
 The Web CGH Software License, Version 1.0
 
@@ -59,7 +59,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.rosuda.JRclient.REXP;
 import org.rosuda.JRclient.RList;
@@ -71,8 +70,8 @@ import org.rti.webcgh.util.IOUtils;
 
 
 /**
- * 
- *
+ * Apply Acgh package in R to smooth AcghData
+ * @author Kungyen
  */
 public class AcghService {
 	private static final Logger LOGGER = Logger.getLogger(AcghService.class);
@@ -82,200 +81,10 @@ public class AcghService {
 		R_COMMAND_LIST = getCommandList();
 	}
 	
-	// URL of RServe
-	private String rserveUrl = "localhost";
-	
 	/**
-	 * @return RServe URL
+	 * Read R commands from file. 
+	 * @return Return a list of R commands
 	 */
-	public String getRserveUrl() {
-		return rserveUrl;
-	}
-
-
-	/**
-	 * @param rServUrl URL of RServe
-	 */
-	public void setRserveUrl(String rServeUrl) {
-		this.rserveUrl = rServeUrl;
-	}
-
-
-
-	/**
-	 * 
-	 */
-	public void run(AcghData data) throws RSrvException {
-		
-		// New Rserve connection
-		LOGGER.info("Trying new Rserve connection to localhost...");	
-		Rconnection c = this.setup(data);
-
-	    int maxChrom = 1;
-	    int[] Chromosomes = data.getChromosomes();
-	    for(int i = 0 ; i < Chromosomes.length ; i++) {
-	      if (Chromosomes[i] > maxChrom) {maxChrom = Chromosomes[i];} 
-	    }
-        String incomplete_cmd = "";
-        boolean isCompleteCmd = true;
-        int sum;
-        // loop through commands and execute each
-		for (String cmd : R_COMMAND_LIST) {
-			
-			// some long commands across lines, to make sure whether cmd is complete
-			if (!isCompleteCmd) {
-				cmd = incomplete_cmd + cmd;
-			}
-			
-			sum = 0;
-			for (int t = 0 ; t < cmd.length() ; t++) {
-				if (cmd.charAt(t) == '(') {
-					sum++;
-				}
-				else if (cmd.charAt(t) == ')') {
-					sum--;
-				}
-			}
-			
-			if (sum%2 == 0) { // cmd is complete
-				isCompleteCmd = true;
-				incomplete_cmd = "";
-			    
-				if (!cmd.equals("") && !cmd.startsWith("#")) {
-					// either blank string or comment is not a valid expression for Rserve
-				    System.out.print("Trying R command: \"" + cmd + "\"...");
-				   
-				    c.voidEval(cmd);
-				    
-				    /*
-				    // find the genomic events with max chrom number 
-				    if (cmd.startsWith("sd.samples")) {
-				    	cmd = "genomic.events(aCGH_obj) <-find.genomic.events(aCGH_obj, maxChrom="+maxChrom+")";
-				    	System.out.print("Trying R command: \"" + cmd + "\"...");
-				    	c.voidEval(cmd);
-				    }
-				    */
-				    
-				    // retrieve smoothed values from R
-				    if (cmd.startsWith("result <-")) {
-				    	RList l = c.eval(cmd).asList();
-				    	//double[] lx = (double[]) l.at(4).getContent(); //observed values
-				    	double[] lx = (double[]) l.at(3).getContent(); //smoothed values			
-				    	//double[] lx = (double[]) l.at(2).getContent(); //states
-				    	//double[] lx = (double[]) l.at(1).getContent(); //kb
-				    	//double[] lx = (double[]) l.at(0).getContent(); //chromosome
-				    	
-				    	data.setSmoothedRatios(lx);
-				    }
-				    
-				    System.out.println("OK");
-				    
-				}
-			}
-			else { // incomplete cmd
-				isCompleteCmd = false;
-				incomplete_cmd = cmd;
-			}
-		}
-		c.close();
-	}
-	
-	
-	private Rconnection setup(AcghData data) {
-		Rconnection c;
-		try {
-			c = new Rconnection(this.rserveUrl);
-			System.out.println("OK!\n");
-			
-			// load aCGH library
-			c.eval("library(aCGH)");
-			
-			/*// < under construction
-			HashMap hCleanIntensity = new HashMap();
-			HashMap hCleanChromosome = new HashMap();
-			HashMap hCleanPosition = new HashMap();
-			String hClone; // key
-			double hLog2;
-			
-			// initiation step
-			hCleanIntensity.put(data.getClones()[0], new Double(data.getLog2Ratios()[0]));
-			hCleanChromosome.put(data.getClones()[0], new Integer(data.getChromosomes()[0]));
-			hCleanPosition.put(data.getClones()[0], new Integer(data.getPositions()[0]));
-			
-			// recursive step
-			for(int i = 1 ; i < data.getSize() ; i++) {
-				hClone = data.getClones()[i];
-				if (hCleanIntensity.containsKey(hClone)) {
-					hLog2 = ((Double)hCleanIntensity.get(hClone)).doubleValue();
-					hLog2 = (hLog2 + data.getLog2Ratios()[i])/2;
-					hCleanIntensity.remove(hClone);
-					hCleanIntensity.put(hClone, new Double(hLog2));
-				}
-				else {
-					hCleanIntensity.put(hClone, new Double(data.getLog2Ratios()[i]));
-					hCleanChromosome.put(hClone, new Integer(data.getChromosomes()[i]));
-					hCleanPosition.put(hClone, new Integer(data.getPositions()[i]));
-				}
-			}
-			
-			// prepare data before running R commands from file
-			int[] narray = new int[hCleanIntensity.size()];
-			String[] clonearray = new String[hCleanIntensity.size()];
-			int[] chrarray = new int[hCleanIntensity.size()];
-			int[] posarray = new int[hCleanIntensity.size()];
-			double[] log2array = new double[hCleanIntensity.size()];
-			Iterator keys = (hCleanIntensity.keySet()).iterator();
-			int m = 0;
-			String clonekey;
-			while (keys.hasNext()) {
-				clonekey = (String)keys.next();
-				narray[m] = m+1;
-				clonearray[m] = clonekey;
-				chrarray[m] = ((Integer)hCleanChromosome.get(clonekey)).intValue();
-				posarray[m] = ((Integer)hCleanPosition.get(clonekey)).intValue();
-				log2array[m] = ((Double)hCleanIntensity.get(clonekey)).doubleValue();
-				m++;
-			}
-			
-			data.setClones(clonearray);
-			data.setTargets(clonearray);
-			data.setChromosomes(chrarray);
-			data.setPositions(posarray);
-			data.setLog2Ratios(log2array);
-			data.setSize(clonearray.length);
-			
-			c.assign("clonesInfo_n",new REXP(REXP.XT_ARRAY_INT,narray));
-			c.assign("clonesInfo_Clone", new REXP(REXP.XT_ARRAY_STR,clonearray));
-			c.assign("clonesInfo_Target", new REXP(REXP.XT_ARRAY_STR,clonearray));
-			c.assign("clonesInfo_Chrom", new REXP(REXP.XT_ARRAY_INT,chrarray));
-			c.assign("clonesInfo_kb", new REXP(REXP.XT_ARRAY_INT,posarray));
-			c.assign("log2ratios_log2TR", new REXP(REXP.XT_ARRAY_DOUBLE,log2array));
-			
-			//   under construction >*/
-			
-			// prepare data before running R commands from file
-			int[] narray = new int[data.getSize()];
-			for(int i = 0 ; i < data.getSize() ; i++) {narray[i] = i+1;}
-			c.assign("clonesInfo_n",new REXP(REXP.XT_ARRAY_INT,narray));
-			c.assign("clonesInfo_Clone", new REXP(REXP.XT_ARRAY_STR,data.getClones()));
-			c.assign("clonesInfo_Target", new REXP(REXP.XT_ARRAY_STR,data.getTargets()));
-			c.assign("clonesInfo_Chrom", new REXP(REXP.XT_ARRAY_INT,data.getChromosomes()));
-			c.assign("clonesInfo_kb", new REXP(REXP.XT_ARRAY_INT,data.getPositions()));
-			c.assign("log2ratios_log2TR", new REXP(REXP.XT_ARRAY_DOUBLE,data.getLog2Ratios()));
-			
-			
-			c.eval("clones_info <-data.frame(n=clonesInfo_n, Clone=clonesInfo_Clone, Target=clonesInfo_Target, Chrom=clonesInfo_Chrom, kb=clonesInfo_kb)");
-			c.eval("log2_ratios <-data.frame(ID=clonesInfo_Clone, log2TR=log2ratios_log2TR)");
-			
-			//c.eval("capture.output(log2_ratios, file = \"C:/test/log2_ratiosx.txt\", append = FALSE)");
-			//c.eval("capture.output(clones_info, file = \"C:/test/clones_infox.txt\", append = FALSE)");
-		} catch (RSrvException e) {
-			throw new WebcghSystemException("Error setting up RServe for aCGH run", e);
-		}
-		return c;
-	}
-	
-	
 	private static List<String> getCommandList() {
 	    BufferedReader rcmdIn = new BufferedReader(new InputStreamReader(
 	    		IOUtils.getInputStream("conf/aCGH_smooth.R")));
@@ -293,4 +102,130 @@ public class AcghService {
 	    return rcmdList;
 	}
 
+	private String rserveUrl = "localhost"; // URL of RServe
+	
+	/**
+	 * @return URL of RServe
+	 */
+	public String getRserveUrl() {
+		return rserveUrl;
+	}
+
+	/**
+	 * @param rServUrl URL of RServe
+	 */
+	public void setRserveUrl(String rServeUrl) {
+		this.rserveUrl = rServeUrl;
+	}
+
+
+
+	/**
+	 * Execute smoothing R commands and retrieve smoothed values. 
+	 * @param data The data given as an AcghData object
+	 * @throws RSrvException if an error occurs during R operation
+	 */
+	public void run(AcghData data) throws RSrvException {
+		LOGGER.info("Trying new Rserve connection to localhost...");	
+		Rconnection c = this.setup(data);
+
+        String incompleteCmd = "";
+        boolean isCompleteCmd = true;
+        int sum;
+        // loop through commands and execute each
+		for (String cmd : R_COMMAND_LIST) {
+			// check whether cmd is finished in one line
+			if (!isCompleteCmd) {
+				cmd = incompleteCmd + cmd;
+			}
+			
+			sum = 0;
+			for (int t = 0 ; t < cmd.length() ; t++) {
+				if (cmd.charAt(t) == '(') {sum++;}
+				else if (cmd.charAt(t) == ')') {sum--;}
+			}
+			
+			if (sum%2 == 0) { // cmd is complete
+				isCompleteCmd = true;
+				incompleteCmd = "";
+				
+				// neither blank string nor comment is a valid cmd for Rserve
+				if (!cmd.equals("") && !cmd.startsWith("#")) {
+				    System.out.print("Trying R command: \"" + cmd + "\"...");
+				    c.voidEval(cmd);
+				    
+				    // cmd which retrieves smoothed values from R
+				    if (cmd.startsWith("result <-")) {
+				    	RList l = c.eval(cmd).asList();
+				    	//double[] lx = (double[]) l.at(4).getContent(); //observed values
+				    	double[] lx = (double[]) l.at(3).getContent(); //smoothed values			
+				    	//double[] lx = (double[]) l.at(2).getContent(); //states
+				    	//double[] lx = (double[]) l.at(1).getContent(); //kb
+				    	//double[] lx = (double[]) l.at(0).getContent(); //chromosome
+				    	
+				    	data.setSmoothedRatios(lx);
+				    }
+				    
+				    System.out.println("OK");
+				}
+			}
+			else { // incomplete cmd
+				isCompleteCmd = false;
+				incompleteCmd = cmd;
+			}
+		}
+		c.close();
+	}
+	
+
+	/**
+	 * Create a new Rserve connection and transform AcghData to data types in R.
+	 * @return Rserve connection
+	 */
+	private Rconnection setup(AcghData data) {
+		Rconnection c;
+		try {
+			c = new Rconnection(this.rserveUrl);
+			System.out.println("OK!\n");
+			
+			// load aCGH library
+			c.eval("library(aCGH)");
+			
+			// prepare data before running R commands from file
+			int[] narray = new int[data.getSize()];
+			for (int i = 0; i < data.getSize(); i++) {
+				narray[i] = i + 1;
+			}
+			c.assign("clonesInfo_n", 
+					new REXP(REXP.XT_ARRAY_INT, narray));
+			c.assign("clonesInfo_Clone", 
+					new REXP(REXP.XT_ARRAY_STR, data.getClones()));
+			c.assign("clonesInfo_Target", 
+					new REXP(REXP.XT_ARRAY_STR, data.getTargets()));
+			c.assign("clonesInfo_Chrom", 
+					new REXP(REXP.XT_ARRAY_INT, data.getChromosomes()));
+			c.assign("clonesInfo_kb", 
+					new REXP(REXP.XT_ARRAY_INT, data.getPositions()));
+			c.assign("log2ratios_log2TR", 
+					new REXP(REXP.XT_ARRAY_DOUBLE, data.getLog2Ratios()));
+			
+			c.eval("clones_info <-data.frame(n=clonesInfo_n, " 
+					+ "Clone=clonesInfo_Clone, Target=clonesInfo_Target, " 
+					+ "Chrom=clonesInfo_Chrom, kb=clonesInfo_kb)");
+			c.eval("log2_ratios <-data.frame(ID=clonesInfo_Clone, "
+				    + "log2TR=log2ratios_log2TR)");
+			
+			/*
+			c.eval("capture.output(log2_ratios, " +
+					"file = \"C:/test/log2_ratiosx.txt\", append = FALSE)");
+			c.eval("capture.output(clones_info, " +
+					"file = \"C:/test/clones_infox.txt\", append = FALSE)");
+			*/
+		} catch (RSrvException e) {
+			throw new WebcghSystemException(
+					"Error setting up RServe for aCGH run", e);
+		}
+		return c;
+	}
+	
 }
