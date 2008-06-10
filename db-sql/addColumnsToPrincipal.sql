f5d4430fc08108efeaa52abe80665400ce386db0
Vesselina Bakalov <vbakalov@rti.org>
2008-06-10 19:15:52 +0000

    *** empty log message ***
    SVN-Revision: 13807

---
+++ db-sql/addColumnsToPrincipal.sql
@@ -0,0 +1,10 @@
+ALTER TABLE principal ADD (first_name VARCHAR2(40));
+ALTER TABLE principal ADD (last_name VARCHAR2(40));
+ALTER TABLE principal ADD (email VARCHAR2(40));
+ALTER TABLE principal ADD (institution VARCHAR2(255));
+ALTER TABLE principal ADD (department VARCHAR2(255));
+ALTER TABLE principal ADD (position VARCHAR2(255));
+ALTER TABLE principal ADD (degree VARCHAR2(40));
+ALTER TABLE principal ADD (phone VARCHAR2(40));
+ALTER TABLE principal ADD (address VARCHAR2(400));
+ALTER TABLE principal ADD (is_feedbacks CHAR(1));
